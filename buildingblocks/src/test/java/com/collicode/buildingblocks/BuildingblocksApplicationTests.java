a8acae99e7302d5ee911c0f1e31df0a46fba98d3
Collins Kiplimo <ckiplimo54@gmail.com>
2025-08-28 12:17:03 +0300

    initial setup

---
+++ buildingblocks/src/test/java/com/collicode/buildingblocks/BuildingblocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collicode.buildingblocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingblocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/flight/src/test/java/com/collicode/flight/FlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collicode.flight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
