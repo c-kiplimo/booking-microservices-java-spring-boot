5915f651c58ee8ef68d43884c66f2ec5718c9416
Collins Kiplimo <ckiplimo54@gmail.com>
2025-08-29 09:44:22 +0300

    add base entity

---
+++ src/buildingblocks/src/main/java/com/collicode/buildingblocks/core/model/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.collicode.buildingblocks.core.model;
+
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@FilterDef(name = "softDeleteFilter", parameters = @ParamDef(name = "isDeleted", type = boolean.class))
+@Filter(name = "softDeleteFilter", condition = "is_deleted = :isDeleted")
+@NoArgsConstructor // Required by JPA
+@AllArgsConstructor
+@Getter
+public abstract class BaseEntity<T> {
+    @Id
+    protected T id;
+    @CreatedDate
+    protected LocalDateTime createdAt;
+    @CreatedBy
+    protected Long createdBy;
+    @LastModifiedDate
+    protected LocalDateTime lastModified;
+    @LastModifiedBy
+    protected Long lastModifiedBy;
+    @Version
+    protected Long version;
+    protected boolean isDeleted = false;
+}
