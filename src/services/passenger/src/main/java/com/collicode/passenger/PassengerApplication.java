104602887143e6bbea6de472e756bf49d20c6fea
Collins Kiplimo <ckiplimo54@gmail.com>
2026-02-09 17:11:47 +0200

    update postgres:db config

--- src/services/passenger/src/main/java/com/collicode/passenger/PassengerApplication.java
+++ src/services/passenger/src/main/java/com/collicode/passenger/PassengerApplication.java
@@ -2,12 +2,14 @@ package com.collicode.passenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PassengerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PassengerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PassengerApplication.class, args);
+    }
 
 }
